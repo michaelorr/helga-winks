8afde44612fe77d58f95abaa63a178a26a8782b0
Michael Orr <michael@orr.co>
2014-08-29 10:58:49 -0400

    people want to wink at poop

--- helga_winks.py
+++ helga_winks.py
@@ -7,8 +7,8 @@ def winks(client, channel, nick, message, cmd, args):
     ~*~*~* winks @ u *~*~*~
     """
     if len(args) == 0:
-        return '~*~*~* {0} winks @ u *~*~*~'.format(nick)
+        return u'~*~*~* {0} winks @ u *~*~*~'.format(nick)
     elif len(args) == 1:
-        return '~*~*~* {0} winks @ {1} *~*~*~'.format(nick, args[0])
+        return u'~*~*~* {0} winks @ {1} *~*~*~'.format(nick, args[0])
     else:
-        return '~*~*~* {0} {1} {2} *~*~*~'.format(nick, args[0], args[1])
+        return u'~*~*~* {0} {1} {2} *~*~*~'.format(nick, args[0], args[1])
